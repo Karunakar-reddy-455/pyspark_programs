faaa3df679e2c5f3ec8e5a42d036128dc7ca2449
Karuna <karunakarreddy.vdc@gmail.com>
2025-09-22 15:46:08 +0530

    third commit

--- create_dataframe.py
+++ create_dataframe.py
@@ -3,5 +3,6 @@ from pyspark.sql.connect.session import SparkSession
 spark = SparkSession.builder.appName("Student").getOrCreate()
 
 data = [
-    (1,"Jagan",)
+    (1,"Jagan",50),
+    (2,"Modi",70)
 ]
