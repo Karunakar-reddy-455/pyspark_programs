6a8f8f767c1322b1e3c1c8748edb9378acbe7fb8
Karuna <karunakarreddy.vdc@gmail.com>
2025-09-22 15:44:13 +0530

    Second commit

--- create_dataframe.py
+++ create_dataframe.py
@@ -1,12 +1,7 @@
-from pyspark.sql import SparkSession
+from pyspark.sql.connect.session import SparkSession
 
-spark = SparkSession.builder.appName("exampleofdataframe").getOrCreate()
+spark = SparkSession.builder.appName("Student").getOrCreate()
 
-data = [(1,'gopi',100),(2,'ayyappa',200),(3,'mahesh',300),(4,'prasanth',400),(5,'bhargav',500)]
-
-columns = ['id','name','salary']
-rdd1 = spark.sparkContext.parallelize(data)
-
-df = spark.createDataFrame(rdd1).toDF(*columns)
-print("dataframe")
-df.show(truncate=False)
+data = [
+    (1,"Jagan",)
+]
